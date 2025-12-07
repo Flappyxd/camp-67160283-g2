2965124d8d4ef264dd559c75a555959b4efbd244
Flappyxd <pent1056216@gmail.com>
2025-12-07 17:03:37 +0700

    ทำต่อ

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,50 @@
+<!-- resources/views/html101.blade.php -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Workshop HTML</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/bootstrap.css" />
+    </head>
+    <body>
+        <style>
+            body{
+                font-family: "Chakra Petch", sans-serif;
+                font-weight: 400;
+
+            }
+            </style>
+        <div class="container pt-5">
+            <h1>Workshop #HTMLr - FORM</h1>
+            <form>
+                <div class="row mt-3">
+                    
+                    <div class="col-4">
+                        <label for="fname" class="col-form-label">
+                        ชื่อ
+</label>
+        </div>
+        <div class="col-6">
+            <input type="text" id="fname" class="form-control">
+</div>
+</div>
+<div class="row">
+    <div class="col-4">
+        <label for="" class="">
+            นามสกุล
+        </label>
+</div>
+<div class="row mt-3">
+    <input type="text" id="fname" class="form-control">
+    </div>
+</div>
+
+</form>
+</div>
+
+    </body>
+</html>
+
